7b4d92201b1a29531036cced6be148d0e16d92ca
Madhavi1508 <baliwadamadhavi@gmail.com>
2026-04-08 22:53:28 +0530

    Create Time_convertor.py

---
+++ Time_convertor.py
@@ -0,0 +1,11 @@
+def convert_minutes(minutes):
+    hours = minutes // 60
+    remaining_minutes = minutes % 60
+
+    if hours > 0:
+        if hours == 1:
+            return f"{hours} hr {remaining_minutes} minutes"
+        else:
+            return f"{hours} hrs {remaining_minutes} minutes"
+    else:
+        return f"{remaining_minutes} minutes"
